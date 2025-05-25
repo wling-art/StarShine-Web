8b524e87a73fe1cdd7e29eff821cf63de570468d
Xk233a <125188926+Xk233a@users.noreply.github.com>
2025-05-25 17:58:05 +0800

    update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,15 +1,10 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '前端导航', link: '/nav/' },
-  { text: '茂茂主页', link: 'https://fe-mm.com' },
+  // { text: '前端导航', link: '/nav/' },
+  { text: '插件服在线地图', link: 'http://mc.8bc.top:20000/' },
   {
-    text: '星光StarShine',
-    link: 'https://notes.fe-mm.com',
-  },
-  { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
-  {
-    text: '油猴脚本',
-    link: 'https://github.com/maomao1996/tampermonkey-scripts',
+    text: '模组服在线地图',
+    link: 'http://mc.8bc.top:27774/',
   },
 ]
